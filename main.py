72cd94d3fa1e48c89051193fff5bade33e9449fa
hakancenkalinoglu <79363052+hakancenkalinoglu@users.noreply.github.com>
2023-03-25 02:46:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ for urls in product_urls:
 		url = "https://public.trendyol.com:443/discovery-web-searchgw-service/v2/api/infinite-scroll/"+urls+"?pi="+str(i)+"&culture=tr-TR&userGenderId=1&pId=0&scoringAlgorithmId=2&categoryRelevancyEnabled=false&isLegalRequirementConfirmed=false&searchStrategyType=DEFAULT&productStampType=TypeA&fixSlotProductAdsIncluded=true&searchAbDecider=%2CSuggestion_A%2CRelevancy_1%2CFilterRelevancy_1%2CListingScoringAlgorithmId_1%2CSmartlisting_2%2CFlashSales_1%2CSuggestionBadges_B%2CProductGroupTopPerformer_A&offset="+str(offset)
 		headers = {"Sec-Ch-Ua": "\"Brave\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"", "Accept": "application/json, text/plain, */*", "Sec-Ch-Ua-Mobile": "?0", "Authorization": "Bearer", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36", "Sec-Ch-Ua-Platform": "\"Linux\"", "Sec-Gpc": "1", "Accept-Language": "en-US,en;q=0.6", "Origin": "https://www.trendyol.com", "Sec-Fetch-Site": "same-site", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Accept-Encoding": "gzip, deflate", "If-None-Match": "W/\"eab6-/qcoADqQInHXa0DL+l016zCl4Zc\""}
 		r = requests.get(url, headers=headers)
-
+		#print(url)
 		try:
 			y = json.loads(r.text)
 			t = y["result"]["products"]
@@ -35,7 +35,17 @@ for urls in product_urls:
 			break
 
 		for variants in y["result"]["products"]:
-			mydict = { "name": categories[cat_id], "price": variants["price"]["sellingPrice"] }
+			mydict = { "name": categories[cat_id], "price": variants["price"]["sellingPrice"], "brand": variants["brand"]["name"]}
+			
+			for var in variants["variants"]:
+				mydict["size"] = var["attributeValue"]
+			
+			if "ratingScore" in variants:
+					mydict["ratingScore"] = variants["ratingScore"]["averageRating"]
+			else:
+					mydict["ratingScore"] = "NULL"
+				
+
 			x = mycol.insert_one(mydict)
 			print(mydict)
 
@@ -44,3 +54,6 @@ for urls in product_urls:
 			offset += 17
 		else:
 			offset += 16
+
+
+
